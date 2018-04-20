93bc807a30c5b1a519cdfa1bf4fbd09fdfa924b6
ycdxsb <1452740432@qq.com>
2018-04-21 00:25:39 +0800

    change test.py

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
-from PyQt5.QtSql import QSqlDatabase,QSqlQuery
-
-db = QSqlDatabase.addDatabase("QSQLITE")
-db.setDatabaseName('./db/LibraryManagement.db')
-db.open()
-query = QSqlQuery()
-sql = "SELECT * FROM user WHERE user.StudentId='%s'" % ("0000000000")
-query.exec_(sql)
-print(query.next())
-print(str(query.value(0)))
+temp="我觉得"
+s="%"
+for i in range(0,len(temp)):
+    s+=temp[i]
+    s+="%"
+print(s)
