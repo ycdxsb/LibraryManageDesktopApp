39054225ce829a95bcac83d874b938aae3404f5e
ycdxsb <1452740432@qq.com>
2018-04-21 17:51:25 +0800

    Merge branch 'master' of github.com:ycdxsb/LibraryManageDesktopApp finish StorageView

--- StudentHome.py
+++ StudentHome.py
@@ -4,14 +4,14 @@ from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import Qt
 import qdarkstyle
 
+
 class StudentHome(QWidget):
     def __init__(self):
         super().__init__()
         self.setUpUI()
 
     def setUpUI(self):
-        self.button=QPushButton()
-
+        self.button = QPushButton()
 
 
 if __name__ == "__main__":
