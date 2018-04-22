469ae62a04ad40987cb2c1897448b434f832f8c6
ycdxsb <1452740432@qq.com>
2018-04-22 21:41:48 +0800

    finished StudentHome

--- StudentHome.py
+++ StudentHome.py
@@ -2,10 +2,12 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import Qt
+import sip
 import qdarkstyle
 from BookStorageViewer import BookStorageViewer
 from borrowBookDialog import borrowBookDialog
 from returnBookDialog import returnBookDialog
+from BorrowStatusViewer import BorrowStatusViewer
 
 
 class StudentHome(QWidget):
@@ -25,26 +27,36 @@ class StudentHome(QWidget):
         self.borrowBookButton = QPushButton("借书")
         self.returnBookButton = QPushButton("还书")
         self.myBookStatus = QPushButton("借阅状态")
+        self.allBookButton = QPushButton("所有书籍")
         self.buttonLayout.addWidget(self.borrowBookButton)
         self.buttonLayout.addWidget(self.returnBookButton)
         self.buttonLayout.addWidget(self.myBookStatus)
+        self.buttonLayout.addWidget(self.allBookButton)
         self.borrowBookButton.setFixedWidth(100)
         self.borrowBookButton.setFixedHeight(42)
         self.returnBookButton.setFixedWidth(100)
         self.returnBookButton.setFixedHeight(42)
         self.myBookStatus.setFixedWidth(100)
         self.myBookStatus.setFixedHeight(42)
+        self.allBookButton.setFixedWidth(100)
+        self.allBookButton.setFixedHeight(42)
         font = QFont()
         font.setPixelSize(16)
         self.borrowBookButton.setFont(font)
         self.returnBookButton.setFont(font)
         self.myBookStatus.setFont(font)
+        self.allBookButton.setFont(font)
 
         self.storageView = BookStorageViewer()
+
+        self.allBookButton.setEnabled(False)
+
         self.layout.addLayout(self.buttonLayout)
         self.layout.addWidget(self.storageView)
         self.borrowBookButton.clicked.connect(self.borrowBookButtonClicked)
         self.returnBookButton.clicked.connect(self.returnBookButtonClicked)
+        self.myBookStatus.clicked.connect(self.myBookStatusClicked)
+        self.allBookButton.clicked.connect(self.allBookButtonClicked)
 
     def borrowBookButtonClicked(self):
         borrowDialog = borrowBookDialog(self.StudentId)
@@ -56,6 +68,24 @@ class StudentHome(QWidget):
         returnDialog = returnBookDialog(self.StudentId)
         returnDialog.show()
         returnDialog.exec_()
+
+    def myBookStatusClicked(self):
+        self.layout.removeWidget(self.storageView)
+        sip.delete(self.storageView)
+        self.borrowStatusView = BorrowStatusViewer(self.StudentId)
+        self.layout.addWidget(self.borrowStatusView)
+        self.allBookButton.setEnabled(True)
+        self.myBookStatus.setEnabled(False)
+        return
+
+    def allBookButtonClicked(self):
+        self.layout.removeWidget(self.borrowStatusView)
+        sip.delete(self.borrowStatusView)
+        self.storageView = BookStorageViewer()
+        self.layout.addWidget(self.storageView)
+        self.allBookButton.setEnabled(False)
+        self.myBookStatus.setEnabled(True)
+        return
 
 
 if __name__ == "__main__":
