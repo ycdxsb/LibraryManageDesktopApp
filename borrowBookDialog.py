0cbd8ad52e1594a34494a04a4c77375c24cb3a47
ycdxsb <1452740432@qq.com>
2018-04-22 15:55:15 +0800

    finish borrowBookDialog

--- borrowBookDialog.py
+++ borrowBookDialog.py
@@ -6,9 +6,11 @@ import qdarkstyle
 import time
 from PyQt5.QtSql import *
 
+
 class borrowBookDialog(QDialog):
-    def __init__(self,StudentId,parent=None):
+    def __init__(self, StudentId, parent=None):
         super(borrowBookDialog, self).__init__(parent)
+        self.studentId = StudentId
         self.setUpUI()
         self.setWindowModality(Qt.WindowModal)
         self.setWindowTitle("借阅书籍")
@@ -22,7 +24,140 @@ class borrowBookDialog(QDialog):
         self.layout = QFormLayout()
         self.setLayout(self.layout)
 
+        # Label控件
+        self.borrowStudentLabel = QLabel("借 阅 人:")
+        self.borrowStudentIdLabel = QLabel(self.studentId)
+        self.titlelabel = QLabel("  借阅书籍")
+        self.bookNameLabel = QLabel("书    名:")
+        self.bookIdLabel = QLabel("书    号:")
+        self.authNameLabel = QLabel("作    者:")
+        self.categoryLabel = QLabel("分    类:")
+        self.publisherLabel = QLabel("出 版 社:")
+        self.publishDateLabel = QLabel("出版日期:")
+
+        # button控件
+        self.borrowBookButton = QPushButton("确认借阅")
+
+        # lineEdit控件
+        self.bookNameEdit = QLineEdit()
+        self.bookIdEdit = QLineEdit()
+        self.authNameEdit = QLineEdit()
+        self.categoryComboBox = QComboBox()
+        self.categoryComboBox.addItems(BookCategory)
+        self.publisherEdit = QLineEdit()
+        self.publishTime = QLineEdit()
+
+        self.bookNameEdit.setMaxLength(10)
+        self.bookIdEdit.setMaxLength(6)
+        self.authNameEdit.setMaxLength(10)
+        self.publisherEdit.setMaxLength(10)
+
+        # 添加进formlayout
+        self.layout.addRow("", self.titlelabel)
+        self.layout.addRow(self.borrowStudentLabel, self.borrowStudentIdLabel)
+        self.layout.addRow(self.bookNameLabel, self.bookNameEdit)
+        self.layout.addRow(self.bookIdLabel, self.bookIdEdit)
+        self.layout.addRow(self.authNameLabel, self.authNameEdit)
+        self.layout.addRow(self.categoryLabel, self.categoryComboBox)
+        self.layout.addRow(self.publisherLabel, self.publisherEdit)
+        self.layout.addRow(self.publishDateLabel, self.publishTime)
+        self.layout.addRow("", self.borrowBookButton)
+
+        # 设置字体
+        font = QFont()
+        font.setPixelSize(20)
+        self.titlelabel.setFont(font)
+        font.setPixelSize(16)
+        self.borrowStudentIdLabel.setFont(font)
+        font.setPixelSize(14)
+        self.borrowStudentLabel.setFont(font)
+        self.bookNameLabel.setFont(font)
+        self.bookIdLabel.setFont(font)
+        self.authNameLabel.setFont(font)
+        self.categoryLabel.setFont(font)
+        self.publisherLabel.setFont(font)
+        self.publishDateLabel.setFont(font)
+
+        self.bookNameEdit.setFont(font)
+        self.bookNameEdit.setReadOnly(True)
+        self.bookIdEdit.setFont(font)
+        self.authNameEdit.setFont(font)
+        self.authNameEdit.setReadOnly(True)
+        self.publisherEdit.setFont(font)
+        self.publisherEdit.setReadOnly(True)
+        self.publishTime.setFont(font)
+        self.categoryComboBox.setFont(font)
+
+        # button设置
+        font.setPixelSize(16)
+        self.borrowBookButton.setFont(font)
+        self.borrowBookButton.setFixedHeight(32)
+        self.borrowBookButton.setFixedWidth(140)
+
+        # 设置间距
+        self.titlelabel.setMargin(8)
+        self.layout.setVerticalSpacing(10)
+        self.borrowBookButton.clicked.connect(self.borrowButtonClicked)
+        self.bookIdEdit.textChanged.connect(self.bookIdEditChanged)
+
+    def borrowButtonClicked(self):
+        # 获取书号，书号为空或不存在库中，则弹出错误
+        # 向Book_User表插入记录，更新User表以及Book表
+        BookId = self.bookIdEdit.text()
+        # BookId为空的处理
+        if (BookId == ""):
+            print(QMessageBox.warning(self, "警告", "你所要借的书不存在，请查看输入", QMessageBox.Yes, QMessageBox.Yes))
+            return
+        # 打开数据库
+        db = db = QSqlDatabase.addDatabase("QSQLITE")
+        db.setDatabaseName('./db/LibraryManagement.db')
+        db.open()
+        query = QSqlQuery()
+        # 如果BookId不存在
+        sql = "SELECT * FROM Book WHERE BookId='%s'" % BookId
+        query.exec_(sql)
+        if (not query.next()):
+            print(QMessageBox.warning(self, "警告", "你所要借的书不存在，请查看输入", QMessageBox.Yes, QMessageBox.Yes))
+            return
+        # 更新User表
+        sql = "UPDATE User SET TimesBorrowed=TimesBorrowed+1,NumBorrowed=NumBorrowed+1 WHERE StudentId='%s'" % self.studentId
+        query.exec_(sql)
+        db.commit()
+        # 更新Book表
+        sql = "UPDATE Book SET NumCanBorrow=NumCanBorrow-1,NumBorrowed=NumBorrowed+1 WHERE BookId='%s'" % BookId
+        query.exec_(sql)
+        db.commit()
+        # 插入User_Book表
+        timenow = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+        sql="INSERT INTO User_Book VALUES ('%s','%s','%s',NULL,1)" %(self.studentId,BookId,timenow)
+        query.exec_(sql)
+        db.commit()
+        print(QMessageBox.information(self,"提示","借阅成功!",QMessageBox.Yes,QMessageBox.Yes))
         return
+
+
+    def bookIdEditChanged(self):
+        bookId = self.bookIdEdit.text()
+        if (bookId == ""):
+            self.bookNameEdit.clear()
+            self.publisherEdit.clear()
+            self.authNameEdit.clear()
+            self.publishTime.clear()
+        db = QSqlDatabase.addDatabase("QSQLITE")
+        db.setDatabaseName('./db/LibraryManagement.db')
+        db.open()
+        query = QSqlQuery()
+        sql = "SELECT * FROM Book WHERE BookId='%s'" % (bookId)
+        query.exec_(sql)
+        # 查询对应书号，如果存在就更新form
+        if (query.next()):
+            self.bookNameEdit.setText(query.value(0))
+            self.authNameEdit.setText(query.value(2))
+            self.categoryComboBox.setCurrentText(query.value(3))
+            self.publisherEdit.setText(query.value(4))
+            self.publishTime.setText(query.value(5))
+        return
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
